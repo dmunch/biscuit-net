3558fef3fb16565329c3525e915d67ad7ee2e424
Daniel Münch <dani.munch@gmail.com>
2022-11-13 20:51:41 +0100

    Remove PublicKey.ComputeHash

--- src/lib/PublicKey.cs
+++ src/lib/PublicKey.cs
@@ -11,27 +11,6 @@ public record PublicKey(Algorithm Algorithm, byte[] Key)
         hashCode.AddBytes(Key);
         return hashCode.ToHashCode();
     }
-    
-    //=> HashCode.Combine(Algorithm, ComputeHash(Key));
-
-    public static int ComputeHash(byte[] data)
-    {
-        unchecked
-        {
-            const int p = 16777619;
-            int hash = (int)2166136261;
-
-            for (int i = 0; i < data.Length; i++)
-                hash = (hash ^ data[i]) * p;
-
-            hash += hash << 13;
-            hash ^= hash >> 7;
-            hash += hash << 3;
-            hash ^= hash >> 17;
-            hash += hash << 5;
-            return hash;
-        }
-    }
 }
 
 public enum Algorithm
